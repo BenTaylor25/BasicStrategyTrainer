593f2bec439a22dadebbd2d2c3b1ea913c8c6a49
Ben Taylor <ben.taylor2573@gmail.com>
2022-11-22 22:00:46 +0000

    user input test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+use std::io::{stdin, Write};
+
 fn main() {
     println!("Hello, world!");
+
+    print!("Enter name: ");
+    io::stdout().flush().unwrap();
+
+    let mut input_string = String::new();
+    stdin().read_line(&mut input_string)
+        .ok()
+        .expect("Failed to read line");
+
+    println!("Hi {}", input_string);
 }
