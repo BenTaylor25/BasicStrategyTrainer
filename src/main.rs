0e2dd5dbe66e66a85cdbbf2506a811408f4b787c
benlaptop25 <ben.taylor2573@gmail.com>
2022-11-30 21:49:33 +0000

    cleared main

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
-use std::io::{self, stdin, Write};
 
-fn int_input(prompt: &str) -> i32 {
-    print!("{}", prompt);
-    io::stdout().flush().unwrap();
-
-    let mut input_string = String::new();
-    stdin().read_line(&mut input_string)
-        .ok()
-        .expect("Failed to read line");
-
-    let int = input_string.trim().parse()
-        .expect("Invalid number");
-
-    int
-}
 
 fn main() {
-    println!("Hello, world!");
-
-    let a = int_input("Enter first number: ");
-    let b = int_input("Enter second number: ");
-
-    let c = a + b;
-
-    println!("{}", c);
+    
 }
