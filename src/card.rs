14bd3b70a4b23319940385559c693451891bfed9
benlaptop25 <ben.taylor2573@gmail.com>
2022-11-30 22:25:54 +0000

    print random card

--- src/card.rs
+++ src/card.rs
@@ -1,47 +1,10 @@
-use rand::{
-    distributions::{Distribution, Standard},
-    Rng,
-};
+use rand::Rng;
 
-pub enum Card {
-    ACE = 'A',
-    ONE = '1',
-    TWO = '2',
-    THREE = '3',
-    FOUR = '4',
-    FIVE = '5',
-    SIX = '6',
-    SEVEN = '7',
-    EIGHT = '8',
-    NINE = '9',
-    TEN = 'T',
-    JACK = 'J',
-    QUEEN = 'Q',
-    KING = 'K'
-}
+pub fn get_rand_card() -> char {
+    let cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
 
-impl Distribution<Card> for Standard {
-    fn sample<R : Rng + ?Sized>(&self, rng: &mut R) -> Card {
-        match rng.gen_range(0..=13) {
-            0 => Card::ACE,
-            1 => Card::ONE,
-            2 => Card::TWO,
-            3 => Card::THREE,
-            4 => Card::FOUR,
-            5 => Card::FIVE,
-            6 => Card::SIX,
-            7 => Card::SEVEN,
-            8 => Card::EIGHT,
-            9 => Card::NINE,
-            10 => Card::TEN,
-            11 => Card::JACK,
-            12 => Card::QUEEN,
-            _ => Card::KING,
-        }
-    }
-}
+    let mut rng = rand::thread_rng();
+    let i = rng.gen_range(0..=13);
 
-fn get_rand_card() -> Card {
-    let card: Card = rand::random();
-    card
+    cards[i]
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-
+mod card;
+use card::{get_rand_card};
 
 fn main() {
-    
+    let c = get_rand_card();
+
+    println!("{}", c);
 }
