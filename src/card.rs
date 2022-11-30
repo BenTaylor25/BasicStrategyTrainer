071ce146f1c2c0622e397d93e3869ce2f02617ec
benlaptop25 <ben.taylor2573@gmail.com>
2022-11-30 21:51:07 +0000

    added Card enum

---
+++ src/card.rs
@@ -0,0 +1,17 @@
+
+pub enum Card {
+    ACE = 'A',
+    ONE = '1',
+    TWO = '2',
+    THREE = '3',
+    FOUR = '4',
+    FIVE = '5',
+    SIX = '6',
+    SEVEN = '7',
+    EIGHT = '8',
+    NINE = '9',
+    TEN = 'T',
+    JACK = 'J',
+    QUEEN = 'Q',
+    KING = 'K'
+}
