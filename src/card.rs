9d39739c4569a593616efe0c2170a1be9186f440
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-01 21:06:50 +0000

    fixed out of range error

--- src/card.rs
+++ src/card.rs
@@ -4,7 +4,7 @@ pub fn get_rand_card() -> char {
     let cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
 
     let mut rng = rand::thread_rng();
-    let i = rng.gen_range(0..=13);
+    let i = rng.gen_range(0..13);
 
     cards[i]
 }
