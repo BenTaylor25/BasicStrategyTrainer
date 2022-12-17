dd502d467ca9ea54ae242c2cde9f4c06fc85be0c
benlaptop25 <ben.taylor2573@gmail.com>
2022-12-17 12:29:30 +0000

    added quit option

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod basic_strategy;
 use std::io::{self, stdin, Write};
 
 fn menu_input(input_string: &mut String) {
-    println!("Hit (h), Stand (s), Double Down (d), or Split (2)");
+    println!("Hit (h), Stand (s), Double Down (d), Split (2), or Quit (q)");
 
     loop {
         print!("> ");
@@ -16,37 +16,42 @@ fn menu_input(input_string: &mut String) {
             .expect("Failed to read input");
 
         match input_string.trim() {
-            "h" | "s" | "d" | "2" => break,
+            "h" | "s" | "d" | "2" | "q" => break,
             _ => continue
         }
     }
 }
 
 fn main() {
-    loop {
+    let mut keep_playing = true;
+    while keep_playing {
         let cards = card::get_rand_deck(3);
         let player_blackjack = 
-            card::get_card_value(cards[1])
-            + card::get_card_value(cards[2])
-            == 21;
+            card::get_card_value(cards[1]) +
+            card::get_card_value(cards[2]) == 21;
 
         display::print_cards(&cards);
 
         if player_blackjack {
             println!("Blackjack!");
+            println!();
         } else {
             let mut input_string = String::new();
             menu_input(&mut input_string);
-
             let input_chars: Vec<char> = input_string.chars().collect();
 
-            let correct_guess = basic_strategy::check_move(&cards, input_chars[0]);
-            if correct_guess {
-                println!("Correct!");
+            if input_chars[0] == 'q' {
+                keep_playing = false;
             } else {
-                println!("Incorrect!");
+                let correct_guess = basic_strategy::check_move(&cards, input_chars[0]);
+                if correct_guess {
+                    println!("Correct!");
+                } else {
+                    println!("Incorrect!");
+                }
+                println!();
             }
         }
-        println!("\n");
+        println!();
     }
 }
